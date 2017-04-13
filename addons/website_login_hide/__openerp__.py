a8e866fe94dfa41d328104cce536807b606ee3b6
rapidgrps <rapidgrps@gmail.com>
2017-04-13 12:52:11 +0600

    Add files via upload

---
+++ addons/website_login_hide/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Hide the "Sign in" button in Website""",
+    "summary": """""",
+    "category": "Website",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    # "price": 20.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "website",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views.xml",
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
